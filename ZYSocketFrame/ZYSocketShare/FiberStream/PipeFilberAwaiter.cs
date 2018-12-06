ba62f114601efcd369da78181c2982877105acd8
luyikk <luyikk@126.com>
2018-12-06 21:55:19 +0800

    format

--- ZYSocketFrame/ZYSocketShare/FiberStream/PipeFilberAwaiter.cs
+++ ZYSocketFrame/ZYSocketShare/FiberStream/PipeFilberAwaiter.cs
@@ -93,59 +93,6 @@ namespace ZYSocket.FiberStream
     }
 
 
-    public class ResultAwaiter<T> : ICriticalNotifyCompletion, INotifyCompletion
-    {
-
-
-        private Action Continuation;
-
-        private T result;
-        internal void Completed()
-        {
-            iscompleted = true;
-            Continuation?.Invoke();
-        }
-
-        internal void Reset()
-        {            
-            iscompleted = false;
-        }
-
-
-        internal void SetResult(T res)
-        {
-            result = res;
-        }
-
-
-
-        private bool iscompleted = false;
-
-        public bool IsCompleted { get { return iscompleted; } }
-
-        public void OnCompleted(Action continuation)
-        {
-            this.Continuation = continuation;
-        }
-
-        public void UnsafeOnCompleted(Action continuation)
-        {
-            this.Continuation = continuation;
-        }
-
-        public ResultAwaiter<T> GetAwaiter() => this;
-
-
-        public T GetResult()
-        {
-            return result;
-        }
-    }
-
-
-    public class StreamInitAwaiter : ResultAwaiter<bool>
-    {
-
-    }
+    
 
 }
